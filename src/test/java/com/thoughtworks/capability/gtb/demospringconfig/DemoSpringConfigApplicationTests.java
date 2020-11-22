2080f7e01aab4d58730a55c0eebdbdeb53b2cf9f
Gin757306533 <757306533@qq.com>
2020-11-22 19:36:36 +0800

    use `TestPropertySource(properties = {"levelNumber=2"})`

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
@@ -1,13 +1,60 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.is;
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoSpringConfigApplicationTests {
 
+	@Autowired
+	MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void should_return_advanced_given_levelNumber_is_1() throws Exception {
+		mockMvc.perform(get("/level"))
+				.andExpect(jsonPath("$", is("advanced")))
+				.andExpect(status().isOk());
+	}
+
+	@SpringBootTest
+	@AutoConfigureMockMvc
+	@TestPropertySource(properties = {"levelNumber=2"})
+	@Nested
+	class LevelNumber2{
+		@Autowired
+		MockMvc mockMvc;
+
+		@Test
+		void should_return_advanced_given_levelNumber_is_2() throws Exception {
+			mockMvc.perform(get("/level"))
+					.andExpect(jsonPath("$", is("advanced")))
+					.andExpect(status().isOk());
+		}
+	}
+
+	@SpringBootTest
+	@AutoConfigureMockMvc
+	@TestPropertySource(properties = {"levelNumber=0"})
+	@Nested
+	class LevelNumber0{
+		@Autowired
+		MockMvc mockMvc;
+
+		@Test
+		void should_return_advanced_given_levelNumber_is_0() throws Exception {
+			mockMvc.perform(get("/level"))
+					.andExpect(jsonPath("$", is("basic")))
+					.andExpect(status().isOk());
+		}
 	}
 
 }
